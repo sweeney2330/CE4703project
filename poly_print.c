11468303cb232e38b10abbdc2334cc60cf5c7b8d
sweeney2330 <sweeney.trevor94@gmail.com>
2014-11-14 19:43:37 +0000

    Create poly_print.c

---
+++ poly_print.c
@@ -0,0 +1,20 @@
+/////////////////////////////////////////////////////////////////////////////////
+//  File: poly_print.c
+//  Desc: prints the polynomial given
+//   
+//  Author: Ian Lodovica
+//  ID: 13131567
+//  Date: 14/11/14
+///////////////////////////////////////////////////////////////////////////////// 
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "adt_header.h"
+
+
+void print_p(polynomial *a){
+  for(int i = 0; i < a->length; i++){
+    printf("%.2lfx^%d ", a->poly[i], i);
+  }
+  printf("\n");
+}
