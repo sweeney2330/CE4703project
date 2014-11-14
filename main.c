4fb1b257a9c7009d7d18f64e894e0d66df4c8c56
Gearóid <gearoidc@hotmail.co.uk>
2014-11-14 17:59:21 +0000

    Update main.c
    Added case to add a polynomial. 
    Case do while doesn't loop when I run it. Leaves after first pass.
    Other small changes. int to double etc.

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
-*
-Main file for our CE4703 project
+/*
+ *Main file for our CE4703 project
 */
-
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -10,6 +9,7 @@ Main file for our CE4703 project
 
 int main()
 {
+/*{
   int i =0;
   int b = 0;
   int create = 1;
@@ -30,20 +30,21 @@ int main()
 	       addCoefficient(i,b,&test);
       }
   }
-
+*/
 
     int sel;
     char choice;
 
   // menu here
     printf("\t\t\tMENU\n");
-    printf("1. Add polynomials\n");
-    printf("2. Subtract polynomials\n");
-    printf("3. Multiply polynomial by scalar\n");
-    printf("4. Divide polynomial by scalar\n");
-    printf("5. Normalise polynomial\n");
-    printf("6. Return order of polynomial\n");
-    printf("7. Print polynomial\n");
+    printf("1. Create a polynomial\n");
+    printf("2. Add polynomials\n");
+    printf("3. Subtract polynomials\n");
+    printf("4. Multiply polynomial by scalar\n");
+    printf("5. Divide polynomial by scalar\n");
+    printf("6. Normalise polynomial\n");
+    printf("7. Return order of polynomial\n");
+    printf("8. Print polynomial\n");
     
     
     do
@@ -51,56 +52,47 @@ int main()
       printf("Enter selection: ");
       scanf("%d", &sel);
       getchar(); 
-
+      polynomial ans,ans2;
+      polynomial poly1;
+      polynomial poly2 = {{4.0,5.0,6.0,7.0,8.0},5};
       switch(sel)
       {
-      case 1: // addition goes here *function calls*
-        ans=  arithmatic(&test,&add, addition);
+      case 1: //creating a polynomial goes here
+	createPolynomial(&poly1);
+	printPolynomial(&poly1);
+	printf("is the polynomial you created\n");
+	break;
+      case 2: // addition goes here *function calls*
+	ans =arithmatic(&poly1,&poly2,addition);
         printPolynomial(&ans);
         printf(" is the result of this addition\n");
         break;
-      case 2: // subtraction goes here
-        ans2=  arithmatic(&test,&add, subtract);
+      case 3: // subtraction goes here
+	ans2=  arithmatic(&poly1,&poly2, subtract);
         printPolynomial(&ans2);
         printf(" is the result of this subtraction\n");
         break;
-      case 3: // multiplying goes here
+      case 4: // multiplying goes here
         break;
-      case 4: // dividing goes here
+      case 5: // dividing goes here
         break;
-      case 5: // Normalise goes here
+      case 6: // Normalise goes here
         break;
-      case 6: // Return order goes here
+      case 7: // Return order goes here
         break;
-      case 7: // printing goes here
+      case 8: // printing goes here
         break;
 
       default: printf("Invalid choice\n");
              
       break;
-    }
-    
-    printf("Do you want to continue?(Y/N): ");
-    scanf("%c", &choice);    
-    getchar();
-  
-  if(choice == 'N' || choice == 'n')
-      system("clear");
-  }while(choice == 'Y' || choice == 'y');
-  //system("clear");
-  return EXIT_SUCCESS;
+      }
+      printf("Do you want to continue?(Y/N): ");
+      scanf("%c", &choice);    
+      getchar();  
+      if(choice == 'N' || choice == 'n')
+	system("clear");
+    }while(choice == 'Y' || choice == 'y');
+    //system("clear");
+    return EXIT_SUCCESS;
 }
-/*  printPolynomial(&test);
-  printf(" is the polynomial entered by you \n");
-  printPolynomial(&add);
-  printf(" is the addition polynomial \n");
-  ans=  arithmatic(&test,&add, addition);
-  printPolynomial(&ans);
-  printf(" is the result of this addition\n");
-  ans2=  arithmatic(&test,&add, subtract);
-  printPolynomial(&ans2);
-  printf(" is the result of this subtraction\n");
-  return EXIT_SUCCESS;
-}
-
-*/
