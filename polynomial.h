6824b5910098b44210ab3e0efdf422c79a4d13e5
sweeney2330 <sweeney.trevor94@gmail.com>
2014-11-14 16:12:23 +0000

    Create polynomial.h

---
+++ polynomial.h
@@ -0,0 +1,27 @@
+#ifndef polynomial_h_
+#define polynomial_h_
+
+#define SIZE 100
+
+//Enum for addition or subtraction arithmetic
+typedef enum {addition,subtract}operation;
+
+//type definition for structure of the polynomial
+typedef struct{
+  double data[SIZE];
+  int next; //Similar to stacking in the next
+} polynomial; //this structure will hold the coefficients of the polynomial
+
+
+//function declarations
+void setupPolynomial (polynomial *p);
+void addCoefficient(int degree, int coefficient, polynomial *p);
+polynomial arithmatic(polynomial *first, polynomial *second,operation a);
+void createPolynomial(int degree, int coefficient, polynomial *p);
+void scalarDivision(polynomial *poly);
+void normalise(polynomial *poly);
+void polynomialOrder(polynomial *poly);
+void printPolynomial(polynomial *poly);
+
+
+#endif
