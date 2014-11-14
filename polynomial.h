b901df72b359ac0236d72acb0d6c6c876bc0a9a0
Gearóid <gearoidc@hotmail.co.uk>
2014-11-14 17:03:27 +0000

    Update polynomial.h
    Removed createPolynomial from older code-> was replaced with setup.
    Changed coefficient to suit other changes to functions.c

--- polynomial.h
+++ polynomial.h
@@ -15,9 +15,8 @@ typedef struct{
 
 //function declarations
 void setupPolynomial (polynomial *p);
-void addCoefficient(int degree, int coefficient, polynomial *p);
+void addCoefficient(int order, double coefficient, polynomial *p);
 polynomial arithmatic(polynomial *first, polynomial *second,operation a);
-void createPolynomial(int degree, int coefficient, polynomial *p);
 void scalarDivision(polynomial *poly);
 void normalise(polynomial *poly);
 void polynomialOrder(polynomial *poly);
