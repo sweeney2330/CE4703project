0276303049a371972fa5e7106c6b0a78ec88cd36
CodexVII <keitaro_ayanokoji@yahoo.ie>
2014-11-15 16:56:19 +0000

    Update poly_print.c
    what's stored in length has now been made equal with the degree of the function.
    since we start i = 0, it has to reach the top

--- poly_print.c
+++ poly_print.c
@@ -2,7 +2,7 @@
 //  File: poly_print.c
 //  Desc: prints the polynomial given
 //   
-//  Author: Ian Lodovica
+//  Author: Group 3
 //  ID: 13131567
 //  Date: 14/11/14
 ///////////////////////////////////////////////////////////////////////////////// 
@@ -13,7 +13,7 @@
 
 
 void print_p(polynomial *a){
-  for(int i = 0; i < a->length; i++){
+  for(int i = 0; i <= a->length; i++){
     printf("%.2lfx^%d ", a->poly[i], i);
   }
   printf("\n");
